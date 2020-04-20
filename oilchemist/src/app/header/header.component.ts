de2effa7c34c73ae67f8b13fd62c2f85e9a2e494
subant05 <anthonycrawford2@mac.co>
2020-04-20 14:25:21 -0500

    Added routing events

--- oilchemist/src/app/header/header.component.ts
+++ oilchemist/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
 
@@ -11,12 +11,18 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit {
   isAuthenticated = false
   userSubscription: Subscription
+  @ViewChild('collapsable') collapsable: ElementRef
 
   constructor(private router: Router,private authService: AuthService) { }
 
   ngOnInit(): void {
     this.userSubscription = this.authService.user.subscribe(user=>{
       this.isAuthenticated = !!user
+      this.router.events.subscribe((event: NavigationEnd)=>{
+        if(event instanceof NavigationEnd){
+          this.collapsable.nativeElement.classList.remove("show")
+        }
+      })
     })
   }
 
